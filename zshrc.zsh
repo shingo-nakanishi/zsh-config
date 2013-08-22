f1ec685d9297729d87fa8b6e957ca33e12efcf13
shingo.nakanishi <shingo.nakanishi@glpgs.com>
2013-08-22 15:41:43 +0900

    my config

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,7 +1,7 @@
 export PATH=.rbenv/shims:/opt/local/bin:/usr/local/bin:/opt/local/sbin:/usr/local/mysql/bin/:/Applications:$PATH
 export SVN_EDITOR="vim"
 export GIT_EDITOR="vim"
-#export PAGER=~/bin/vimpager
+export PAGER=~/bin/vimpager
 export LANG=ja_JP.UTF-8
 WORDCHARS=${WORDCHARS:s,/,,}
 
@@ -15,9 +15,9 @@ setopt pushd_ignore_dups  #重複するなら古い方を消す
 ################################################################
 # 分割したファイルの読み込み
 ################################################################
-#source $ZDOTDIR/lib/z-prompt.zsh
+source $ZDOTDIR/lib/z-prompt.zsh
 source $ZDOTDIR/lib/z-complement.zsh
-#source $ZDOTDIR/lib/z-ruby.zsh
+source $ZDOTDIR/lib/z-ruby.zsh
 source $ZDOTDIR/lib/z-common-alias.zsh
 source $ZDOTDIR/lib/z-my-function.zsh
 source $ZDOTDIR/lib/z-emacs-bind.zsh
