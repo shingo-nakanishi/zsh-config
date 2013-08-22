b7a76b443f6f91eacca67e0a8cb8824a51ec2076
shingo.nakanishi <shingo.nakanishi@glpgs.com>
2013-08-22 14:17:18 +0900

    changed file name

--- lib/z-zmv.sh
+++
@@ -1,8 +0,0 @@
-echo "include z-zmv.sh"
-
-################################################################
-# zmv, zcp
-################################################################
-autoload -Uz zmv
-alias zmv='noglob zmv -W'
-alias zcp='noglob zmv -W -C'
