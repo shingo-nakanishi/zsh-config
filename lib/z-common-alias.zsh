f5ff9471e3b09a6ffe47eb26b9ae46da2dbcfce5
shingo.nakanishi <shingo.nakanishi@glpgs.com>
2013-08-29 11:48:27 +0900

    エイリアス追加

--- lib/z-common-alias.zsh
+++ lib/z-common-alias.zsh
@@ -5,4 +5,5 @@ echo "include z-common-alias.zsh"
 ################################################################
 alias ll="ls -l"
 alias la="ls -la"
+alias cd-iPhoneSimulator="~/Library/Application Support/iPhone Simulator"
 #alias di="dirs | tr ' ' '\n'"
