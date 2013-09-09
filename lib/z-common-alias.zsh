73a1cadd47db6b85549a02758e60bf095c62758d
shingo.nakanishi <shingo.nakanishi@glpgs.com>
2013-09-09 10:47:59 +0900

    add alias

--- lib/z-common-alias.zsh
+++ lib/z-common-alias.zsh
@@ -5,5 +5,8 @@ echo "include z-common-alias.zsh"
 ################################################################
 alias ll="ls -l"
 alias la="ls -la"
+alias jj="jobs"
+alias hh="history-all"
+alias ff="fg"
 alias cd-iPhoneSimulator="~/Library/Application Support/iPhone Simulator"
 #alias di="dirs | tr ' ' '\n'"
