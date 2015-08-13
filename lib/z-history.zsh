7e8b64da22b57d15c46fe8d01abba3de6cb120e5
shingo.nakanishi <shingo7ka24@gmail.com>
2015-08-13 14:02:05 +0900

    historyの便利コマンド追加

--- lib/z-history.zsh
+++ lib/z-history.zsh
@@ -6,6 +6,12 @@ echo "include z-history.zsh"
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
-function history-all { history -E 1 } # 全履歴の一覧を出力する
+
 setopt hist_ignore_dups # ダブった履歴を無視する
 setopt share_history    # ヒストリを共有する
+
+alias hg="history 1| tr -s ' ' | cut -d' ' -f3- | grep" 
+
+function hgc(){
+	hg $1 | sort -r | head -1 | tr -d '\n' | pbcopy
+}
