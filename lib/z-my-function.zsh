826456e375df83754983e0458c2a3f595539985a
shingo.nakanishi <shingo7ka24@gmail.com>
2015-08-22 23:08:51 +0900

    today関数追加

--- lib/z-my-function.zsh
+++ lib/z-my-function.zsh
@@ -10,3 +10,14 @@ pjfind (){
 iPhoneEmulatorFind (){
   find ~'/Library/Application Support/iPhone Simulator' -name '*' -print0 | xargs -0 grep "$1" /dev/null
 }
+
+today (){
+  perl -e 'use POSIX 'strftime';
+           $dir_name = strftime("%Y%m%d", localtime);
+           mkdir $dir_name or die $!;'
+}
+
+cd_today (){
+  D=`date +%Y%m%d`;
+  cd ./$D;
+}
